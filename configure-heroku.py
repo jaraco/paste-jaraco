98c0e65183e13f6c2a4b378b15e856e8f94eea39
Jason R. Coombs <jaraco@jaraco.com>
2012-06-17 21:41:32 -0400

    Added code for installing the domain name

--- configure-heroku.py
+++ configure-heroku.py
@@ -1,17 +1,26 @@
+from __future__ import print_function
+
+import sys
 import urllib2
 import json
 import pprint
+import urlparse
 
 import keyring.http
 import jaraco.net.http
+
+def url(subpath):
+	#return urlparse.urljoin('http://localhost:8080/apps/past-jaraco', subpath)
+	return urlparse.urljoin('https://api.heroku.com/apps/paste-jaraco/',
+		subpath)
 
 class FixedUserKeyringPasswordManager(keyring.http.PasswordMgr):
 	def __init__(self, username):
 		self.username = username
 
 	def get_username(self, realm, authuri):
-		print 'realm is', realm
-		print 'authuri is', authuri
+		print('realm is', realm)
+		print('authuri is', authuri)
 		return self.username
 
 	# provide clear_password until delete_password is officially
@@ -35,7 +44,7 @@ def add_MongoHQ():
 		'Accept': 'application/json',
 	}
 	req = jaraco.net.http.MethodRequest(
-		url = 'https://api.heroku.com/apps/paste-jaraco/addons/mongohq',
+		url = url('addons/mongohq'),
 		method = 'POST', headers=headers,
 	)
 	install_opener()
@@ -43,5 +52,30 @@ def add_MongoHQ():
 	assert res.code == 200
 	pprint.pprint(json.loads(res.read()))
 
+def add_hostname():
+	headers = {
+		'Accept': 'application/json',
+	}
+	data = 'domain_name[domain]=paste.jaraco.com'
+	install_opener()
+
+	req = jaraco.net.http.MethodRequest(
+		url=url('domains'), headers=headers, data=data, method='POST')
+	try:
+		res = urllib2.urlopen(req)
+	except urllib2.HTTPError as err:
+		errors = json.loads(err.read())
+		if isinstance(errors, list):
+			for error in errors:
+				print(':'.join(error), file=sys.stderr)
+		else:
+			print(pprint.pformat(errors), file=sys.stderr)
+		return
+
+	assert 200 <= res.code < 300
+	pprint.pprint(json.loads(res.read()))
+
+
 if __name__ == '__main__':
 	add_MongoHQ()
+	add_hostname()
